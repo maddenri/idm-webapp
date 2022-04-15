431d881c90964fa371d3a5a3527d2b33c5104e0b
maddenri <maddenri@tcd.ie>
2022-04-15 17:16:51 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import myPowerPoint from "./files/CloudPresentation.pptx";
+import myPDF from "./files/Madden_Richard.pdf";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+  <React.Fragment>
+    <h1>Systems Architecture - Assigment 2</h1>
+    <h3>Files:</h3>
+    <ul>
+      <li><a href={myPowerPoint}>Q1 - Cloud Presentation</a></li>
+      <li><a href={myPDF}>Q2,Q3,Q4 - Identity Management Review</a></li>
+    </ul>
+  </React.Fragment>
+  );
 }
 
 export default App;
